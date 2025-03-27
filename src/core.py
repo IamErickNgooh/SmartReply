113f55b73faea2b81f2bea57c9a8b2a44bfa4116
Erick-Ngooh <erickngooh@gmail.com>
2025-03-27 14:09:33 +0100

    feat: add function create_session_if_not_exists

--- src/core.py
+++ src/core.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from pydantic_ai.agent import Agent
+from pydantic_ai.messages import ModelMessage
 
 load_dotenv()
 
@@ -7,6 +8,13 @@ def create_agent(model: str, prompt: str) -> Agent:
     agent: Agent = Agent(model=model, system_prompt=prompt)
     return agent
 
-agent = create_agent('groq:gemma2-9b-it', 'You is powerfull assitante for task life')
-result = agent.run_sync('Hello')
-print(result.data)
+store: dict[str, list[bytes]] = {}
+
+def create_session_if_not_exists(session_id: str) -> None:
+    if session_id not in store:
+        store[session_id]: list[ModelMessage] = [] # type: ignore
+
+    
+#agent = create_agent('groq:gemma2-9b-it', 'You is powerfull assitante for task life')
+#result = agent.run_sync('Hello')
+#print(result.data)
