dee8931feb210547eb104aa2014847a0e7fcbd73
Erick-Ngooh <erickngooh@gmail.com>
2025-03-27 14:00:28 +0100

    feat: add create_agent function that to create a instance of agent

--- src/core.py
+++ src/core.py
@@ -3,7 +3,10 @@ from pydantic_ai.agent import Agent
 
 load_dotenv()
 
-agent = Agent(model='groq:gemma2-9b-it', system_prompt='You is powerfull assitante for task life')
+def create_agent(model: str, prompt: str) -> Agent: 
+    agent: Agent = Agent(model=model, system_prompt=prompt)
+    return agent
+
+agent = create_agent('groq:gemma2-9b-it', 'You is powerfull assitante for task life')
 result = agent.run_sync('Hello')
 print(result.data)
-
