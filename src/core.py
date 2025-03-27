8961c69db6a7111c3b743d29a6962b3affc793be
Erick-Ngooh <erickngooh@gmail.com>
2025-03-27 14:37:26 +0100

    feat: add function get_chat_history

--- src/core.py
+++ src/core.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 from pydantic_ai.agent import Agent
-from pydantic_ai.messages import ModelMessage
+from pydantic_ai.messages import ModelMessage, ModelMessagesTypeAdapter
 from pydantic_ai.agent import AgentRunResult
+from itertools import chain
 
 
 load_dotenv()
@@ -19,10 +20,18 @@ def create_session_if_not_exists(session_id: str) -> None:
 def store_messages_in_history(session_id: str, message: AgentRunResult[ModelMessage]) -> None:
     create_session_if_not_exists(session_id)
     store[session_id].append(message.new_messages_json())
+
+def get_chat_history(session_id: str) -> list[ModelMessage]:
+    create_session_if_not_exists(session_id)
+    return list(chain.from_iterable(
+        ModelMessagesTypeAdapter.validate_json(msg_group)
+        for msg_group in store[session_id]
+    ))
+ 
  
 agent = create_agent('groq:gemma2-9b-it', 'You is powerfull assitante for task life')
 result = agent.run_sync('Hello')
 store_messages_in_history('user123', result)
-
-for _, value in store.items():
-    print("test _____ ", value)
+result = agent.run_sync('What is hello world')
+store_messages_in_history('user123', result)
+print(get_chat_history('user123'))
