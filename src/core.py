16d173efd962870962f5b4eff58a1061ac89a14c
Erick-Ngooh <erickngooh@gmail.com>
2025-03-27 14:16:36 +0100

    feat: add function store_messages_in_history

--- src/core.py
+++ src/core.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 from pydantic_ai.agent import Agent
 from pydantic_ai.messages import ModelMessage
+from pydantic_ai.agent import AgentRunResult
+
 
 load_dotenv()
 
@@ -14,7 +16,13 @@ def create_session_if_not_exists(session_id: str) -> None:
     if session_id not in store:
         store[session_id]: list[ModelMessage] = [] # type: ignore
 
-    
-#agent = create_agent('groq:gemma2-9b-it', 'You is powerfull assitante for task life')
-#result = agent.run_sync('Hello')
-#print(result.data)
+def store_messages_in_history(session_id: str, message: AgentRunResult[ModelMessage]) -> None:
+    create_session_if_not_exists(session_id)
+    store[session_id].append(message.new_messages_json())
+ 
+agent = create_agent('groq:gemma2-9b-it', 'You is powerfull assitante for task life')
+result = agent.run_sync('Hello')
+store_messages_in_history('user123', result)
+
+for _, value in store.items():
+    print("test _____ ", value)
