db4b08008cb510fa882e1ec2e281bac827bb70b1
Erick-Ngooh <erickngooh@gmail.com>
2025-03-27 13:52:00 +0100

    feat: add keys aapi config and install groq package

--- src/core.py
+++ src/core.py
@@ -1,2 +1,9 @@
-def hello(name: str) -> str:
-    return f"Hello, {name}!"
+from dotenv import load_dotenv
+from pydantic_ai.agent import Agent
+
+load_dotenv()
+
+agent = Agent(model='groq:gemma2-9b-it', system_prompt='You is powerfull assitante for task life')
+result = agent.run_sync('Hello')
+print(result.data)
+
