05062535313aa4afd97114edbaa6553208a87cc8
Erick-Ngooh <erickngooh@gmail.com>
2025-03-28 13:45:58 +0100

    feat: add class smart reply, a class that create history converstion in agent

--- src/core.py
+++ src/core.py
@@ -1,37 +1,51 @@
+import secrets
+from itertools import chain
 from dotenv import load_dotenv
 from pydantic_ai.agent import Agent
-from pydantic_ai.messages import ModelMessage, ModelMessagesTypeAdapter
 from pydantic_ai.agent import AgentRunResult
-from itertools import chain
-
+from pydantic_ai.messages import ModelMessage, ModelMessagesTypeAdapter
 
 load_dotenv()
 
-def create_agent(model: str, prompt: str) -> Agent: 
-    agent: Agent = Agent(model=model, system_prompt=prompt)
-    return agent
+class SmartReply:
+    store: dict[str, list[bytes]] = {}
+    agent: Agent = None
+    _session_id: str = 0
 
-store: dict[str, list[bytes]] = {}
+    def __init__(self, model: str, prompt: str):
+        self._create_session()
+        self.agent = Agent(model=model, system_prompt=prompt)
+        self.agent.run_sync('he')
 
-def create_session_if_not_exists(session_id: str) -> None:
-    if session_id not in store:
-        store[session_id]: list[ModelMessage] = [] # type: ignore
+    def _create_session(self) -> None:
+        self._session_id: str = secrets.token_hex(16)
+        self.store[self._session_id]: list[ModelMessage] = [] # type: ignore
+     
+    def _get_session(self) -> None:
+        return self._session_id
+    
+    def ask_user(self, user_message: str) -> str:
+        chat_history = self.get_history()
+        response = self.agent.run_sync(user_message, message_history=chat_history)
+        self.store_message_in_history(response)
+        return response.data
+    
+    def store_message_in_history(self, result: AgentRunResult[ModelMessage]) -> None:
+        session_id = self._get_session()
+        self.store[session_id].append(result.new_messages_json())
+    
+    
+    def get_history(self) -> list[ModelMessage]:
+        return list(
+            chain.from_iterable(
+                ModelMessagesTypeAdapter.validate_json(msg_group)
+            for msg_group in self.store[self._get_session()]
+        )
+    )
 
-def store_messages_in_history(session_id: str, message: AgentRunResult[ModelMessage]) -> None:
-    create_session_if_not_exists(session_id)
-    store[session_id].append(message.new_messages_json())
 
-def get_chat_history(session_id: str) -> list[ModelMessage]:
-    create_session_if_not_exists(session_id)
-    return list(chain.from_iterable(
-        ModelMessagesTypeAdapter.validate_json(msg_group)
-        for msg_group in store[session_id]
-    ))
- 
- 
-agent = create_agent('groq:gemma2-9b-it', 'You is powerfull assitante for task life')
-result = agent.run_sync('Hello')
-store_messages_in_history('user123', result)
-result = agent.run_sync('What is hello world')
-store_messages_in_history('user123', result)
-print(get_chat_history('user123'))
+ask = SmartReply('groq:gemma2-9b-it', 'You is powerfull assitante for task life')
+ask._get_session()
+print(ask.ask_user('hello'))
+print(ask.ask_user('my name is erick '))
+print(ask.ask_user('What my name ? '))
